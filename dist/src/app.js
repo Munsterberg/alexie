8dad5944b5d1066acc87ffd248998a7b50fcfb70
Munsterberg <jetrussell93@gmail.com>
2017-10-30 22:03:16 -0400

    fix 3

---
+++ dist/src/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.get('/', function (req, res) {
+  res.send('hi');
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
