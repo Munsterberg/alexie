1cf449df9c3ea8a0c01abe157bef816245035007
Munsterberg <jetrussell93@gmail.com>
2017-11-02 15:17:24 -0400

    setup react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Dashboard = () => <div><h1>Dashboard</h1></div>;
+const NotFoundPage = () => <div>Page Not Found</div>;
 
 const App = () => {
   return (
-    <div>
-      Hi
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/' component={Dashboard} exact={true} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
