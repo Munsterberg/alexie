53bb0091924a3defd8aae03ba802c20421db3526
Munsterberg <jetrussell93@gmail.com>
2017-11-02 11:57:46 -0400

    change server port

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
