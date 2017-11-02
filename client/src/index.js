9b7a1e37a646e760de0d1fffecb535c0532beed6
Munsterberg <jetrussell93@gmail.com>
2017-11-02 15:00:54 -0400

    create basic reducer and add redux ext

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-import App from './App';
+import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(
+    applyMiddleware()
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
