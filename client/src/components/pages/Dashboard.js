e039d4542f2791256817f73957933d7f85b5da1c
Munsterberg <jetrussell93@gmail.com>
2017-11-07 14:24:05 -0500

    refactor search form

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -3,50 +3,22 @@ import { connect } from 'react-redux';
 import { func } from 'prop-types';
 
 import { fetchSummoner } from '../../actions/index';
+import SearchForm from '../SearchForm';
 
 class Dashboard extends React.Component {
-  state = {
-    summonerName: ''
-  }
-
   static propTypes = {
     fetchSummoner: func
   }
 
-  _onInputChange = (e) => {
-    const element = e.target;
-    const stateObj = {};
-
-    stateObj[element.name] = element.value;
-    this.setState(() => stateObj);
-  }
-
-  _onSubmit = (e) => {
-    e.preventDefault();
-
-    this.props.fetchSummoner(this.state.summonerName);
-    console.log('submitted');
+  _fetchSummoner = (summonerName) => {
+    this.props.fetchSummoner(summonerName);
   }
 
   render() {
-    const { summonerName } = this.state;
     return (
       <div className="container">
         <h1>Search for a Summoner</h1>
-        <div className="row">
-          <form className="col s12" onSubmit={this._onSubmit}>
-            <input
-              type="text"
-              className="validate"
-              placeholder="Munsterberg"
-              value={summonerName}
-              onChange={this._onInputChange}
-              name="summonerName"
-              id="summonerName"
-            />
-            <button className="waves-effect waves-light btn">Search</button>
-          </form>
-        </div>
+        <SearchForm onSubmit={this._fetchSummoner} />
       </div>
     );
   }
